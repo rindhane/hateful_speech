3059b810e2abe7d88b2c703c914dc097ea561aea
irahul <29147602+rindhane@users.noreply.github.com>
2020-01-14 17:57:20 +0530

    improved csv reading file reading_data

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hateful_speech
 An library to detect hateful speech detection
+
+
+### Data_reference:
+ Link = https://github.com/t-davidson/hate-speech-and-offensive-language/tree/master/data
+
--- reading_data.java
+++ reading_data.java
@@ -2,55 +2,59 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import com.opencsv.*;
 import java.util.List;
+import java.util.ArrayList;
 
-public class reading_data{
-    public static void main(String[] args) throws ClassNotFoundException
+
+
+public class trial
+{
+    static ArrayList<String> comp_string(FileReader ar, int cols, char separator) throws Exception 
+    {
+        int al;
+        char foo;
+        String hold_string;
+        ArrayList<String> string_set = new ArrayList<String>();
+        char test_end = separator;
+        for (int i= 0; i< cols; i++ ) 
+        {
+            hold_string="";
+            do 
+            {
+                test_end = (i<cols-1)? separator :'\n';
+                al = ar.read();
+                foo = (char) al ;
+                if (foo == test_end) 
+                {
+                    continue;
+                }else {
+                    hold_string = hold_string + Character.toString(foo);
+                }
+                
+            }while(foo != test_end);
+            string_set.add(hold_string);
+        }
+        return string_set;
+    }
+
+
+    public static void main (String [] args) throws Exception
     {
         String csvFile ="C:\\Java\\hateful_speech\\data.csv";
-        BufferedReader br = null ;
-        String line = "";
-        String cvsSplitBy = ",";
-
-        try {
-            br =new BufferedReader(new FileReader(csvFile));
-            String [] nextRecord;
-            int i=0;
-            while (i<20)
+        int no_of_cols=7;
+        char csvSplitBy = ',';
+        FileReader br =new FileReader(csvFile);
+        ArrayList<String> catch_text =null;
+        for (int i =0 ; i< 5; i++)
+        {
+            catch_text = comp_string(br,no_of_cols,csvSplitBy);
+            for (String items : catch_text)
             {
-                line=br.readLine();
-                nextRecord =line.split(cvsSplitBy);
-                for (String cell :nextRecord){
-                    System.out.print(cell +"\t");
-                }
-                System.out.println();
-                i++;
+                System.out.print(items + '\t');
             }
-            //List <String[]> allData =csvreader.readAll() ;
-            /*for (String[] row : allData) 
-            {
-                for (String cell : row )
-                {
-                    System.out.print(cell+ "\t");
-
-                }
-                // printing empty separator line
-                System.out.println();
-            }*/
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }catch(IOException e){
-            e.printStackTrace();
-        }finally {
-            if (br !=null) {
-                try {
-                    br.close();
-                }catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
+        }    
+        System.out.println();
+        System.out.println(catch_text.size());
+        br.close();
     }
 }
